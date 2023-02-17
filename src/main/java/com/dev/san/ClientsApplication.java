8690dfaad2ee0af434a238bf10b7477ede2a82bc
Sanderson F. de Moura <sandersonferraz@gmail.com>
2023-02-16 23:11:18 -0300

    Added client entity with  Entity and Data annotation

---
+++ src/main/java/com/dev/san/ClientsApplication.java
@@ -0,0 +1,11 @@
+package com.dev.san;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dev/san/model/entity/Client.java
@@ -0,0 +1,21 @@
+package com.dev.san.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 150)
+    private String name;
+    @Column(nullable = false, length = 11)
+    private String cpf;
+    @Column
+    private LocalDate registerDate;
+
+}
