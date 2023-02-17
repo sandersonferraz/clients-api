2f9eddfe5f3a42e334695f9f72811f79d69e1d45
Sanderson F. de Moura <sandersonferraz@gmail.com>
2023-02-16 23:26:50 -0300

    Added client and service repository

---
+++ src/main/java/com/dev/san/model/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.dev.san.model.repository;
+
+import com.dev.san.model.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+}
---
+++ src/main/java/com/dev/san/model/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.dev.san.model.repository;
+
+import com.dev.san.model.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+}
